02fc6d2d39ef9366c3ea72a1e31314ca99c137e5
Brian Larson <brl@multitude.net>
2022-10-27 09:21:17 -0500

    try to get info markers for connections to appear

--- bless/src/com/multitude/bless/exceptions/BLESSmarker.java
+++ bless/src/com/multitude/bless/exceptions/BLESSmarker.java
@@ -6,6 +6,7 @@ import org.osate.aadl2.Connection;
 import org.osate.aadl2.NamedElement;
 import org.osate.aadl2.instance.ConnectionInstance;
 import org.osate.aadl2.instance.ConnectionInstanceEnd;
+import org.osate.aadl2.instance.InstanceObject;
 import org.osate.aadl2.modelsupport.errorreporting.AnalysisErrorReporterManager;
 import org.osate.aadl2.modelsupport.errorreporting.MarkerAnalysisErrorReporter;
 
@@ -67,7 +68,7 @@ setInfoMarker(String message, BAST node)
   	}  //end of setInfoMarker
 
   public static void  
-setConnectionInfoMarker(String message, ConnectionInstanceEnd con)
+setConnectionInfoMarker(String message, InstanceObject con)
     {
     if (errManager==null)
       errManager = new AnalysisErrorReporterManager(
