f3203e9be0ffb885208305b4c3c4384f197ba556
Brian Larson <brl@multitude.net>
2024-11-01 18:47:58 -0500

    org.osate.aadl2.modelsupport;bundle-version="[7.0.0,9.0.0)",

--- com.multitude.aadl.bless.ui/xtend-gen/com/multitude/aadfl/bless/ui/activator/MyBlessActivator.java
+++
@@ -1,7 +0,0 @@
-package com.multitude.aadfl.bless.ui.activator;
-
-import com.multitude.aadl.bless.ui.internal.BlessActivator;
-
-@SuppressWarnings("all")
-public class MyBlessActivator extends BlessActivator {
-}
